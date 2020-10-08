725721bc237915fef29d178d30526e0646002c42
Julián <julian.penaranda@aeioros.com>
2020-10-08 21:27:43 +0200

    Added dotenv config for launch server

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
+
+if (ENV === "development") {
+  console.log("Enviroment: ", "Working on develop");
+} else if (ENV === "production") {
+  console.log("Enviroment: ", "Working on production");
+}
 
 app.get("*", (request, response) => {
   response.send({ hello: "express" });
 });
 
-app.listen(3000, (error) => {
+app.listen(PORT, (error) => {
   if (error) {
     console.log("Error: ", "can not run the server.");
   } else {
