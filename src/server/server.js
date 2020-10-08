5b1e78da1a313c43959a7f4a7ed16b399dc80826
Julián <julian.penaranda@aeioros.com>
2020-10-08 22:23:16 +0200

    Serving react APP from Webpack

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import webpack from "webpack";
 
 dotenv.config();
 
@@ -8,12 +9,38 @@ const app = express();
 
 if (ENV === "development") {
   console.log("Enviroment: ", "Working on develop");
+
+  const webpackConfig = require("../../webpack.config");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const compiler = webpack(webpackConfig);
+
+  const webpackServerConfig = {
+    port: ENV,
+    hot: true,
+  };
+
+  app.use(webpackDevMiddleware(compiler, webpackServerConfig));
+  app.use(webpackHotMiddleware(compiler));
 } else if (ENV === "production") {
   console.log("Enviroment: ", "Working on production");
 }
 
 app.get("*", (request, response) => {
-  response.send({ hello: "express" });
+  response.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <link href="assets/app.css" rel="stylesheet" type="text/css">
+      <title>React App</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script src="assets/app.js" type="text/javascript"></script>
+    </body>
+  </html>
+  `);
 });
 
 app.listen(PORT, (error) => {
