1319a100029a5151875d8b9a3e8f331765cd1538
Julián <julian.penaranda@aeioros.com>
2020-10-09 22:53:30 +0200

    Optimizing finished build for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 require("dotenv").config();
 
@@ -19,9 +21,14 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "src/server/public"),
     filename: "assets/app.js",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   module: {
     rules: [
@@ -54,8 +61,17 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
     isDev ? new webpack.HotModuleReplacementPlugin() : () => {},
+    isDev
+      ? () => {}
+      : new CompressionWebpackPlugin({
+          test: /\.js$|\.css$/,
+          filename: "[path].gz",
+        }),
     new MiniCssExtractPlugin({
       filename: "assets/app.css",
     }),
