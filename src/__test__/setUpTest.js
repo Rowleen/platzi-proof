7443182de3c351ac60b21489c81e139d27aaa21d
Julián <julian.penaranda@aeioros.com>
2020-10-18 15:46:56 +0200

    MOVED jest config from json to package.json

--- src/__test__/setUpTest.js
+++ src/__test__/setUpTest.js
@@ -2,4 +2,6 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+// To test promises based on request
 global.fetch = require("jest-fetch-mock");
