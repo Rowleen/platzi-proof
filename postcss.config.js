bdf84d6c668d10ec93f5d610cf964a620e7e6966
Rowleen <julianpeacevedo@gmail.com>
2020-11-19 17:53:00 +0100

    FIXED autoprefixer didnt load

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const autoprefixer = require("autoprefixer");
 module.exports = {
   plugins: [
     autoprefixer({
-      browsers: ["last 2 versions", "IE >= 9", "safari >= 8"],
+      overrideBrowserslist: ["last 2 versions", "IE >= 9", "safari >= 8"],
     }),
   ],
 };
