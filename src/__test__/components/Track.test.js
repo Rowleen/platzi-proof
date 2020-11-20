e0e63257ea446477d5418efcf518206613459222
Rowleen <julianpeacevedo@gmail.com>
2020-11-20 11:31:00 +0100

    DELETED test because component Pill doesn't exists

--- src/__test__/components/Track.test.js
+++ src/__test__/components/Track.test.js
@@ -3,7 +3,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import genresMock from "../../__mocks__/genresMock";
 
-import { Track, Pill } from "components";
+import { Track } from "components";
 
 describe("Testing renderin and UI of <Track />", () => {
   const getLyric = jest.fn();
@@ -13,10 +13,6 @@ describe("Testing renderin and UI of <Track />", () => {
 
   test("Check if the component render", () => {
     expect(track.length).toEqual(1);
-  });
-
-  test("Check if the component render the genres", () => {
-    expect(track.find(Pill)).toHaveLength(3);
   });
 });
 
