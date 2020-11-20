4efb34266b11a62245a2e6cf8af00315be9382e9
Rowleen <julianpeacevedo@gmail.com>
2020-11-20 22:42:35 +0100

    DELETED test that is not nescesary

--- src/__test__/components/Track.test.js
+++ src/__test__/components/Track.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import React from "react";
+import ProviderMock from "../../__mocks__/providerMock";
 import { shallow } from "enzyme";
 import genresMock from "../../__mocks__/genresMock";
 
@@ -8,22 +9,12 @@ import { Track } from "components";
 describe("Testing renderin and UI of <Track />", () => {
   const getLyric = jest.fn();
   const track = shallow(
-    <Track id={1} genres={genresMock} getLyric={getLyric} />
+    <ProviderMock>
+      <Track id={1} genres={genresMock} getLyric={getLyric} />
+    </ProviderMock>
   );
 
   test("Check if the component render", () => {
     expect(track.length).toEqual(1);
-  });
-});
-
-describe("Check if functions works", () => {
-  const getLyric = jest.fn();
-  const track = shallow(
-    <Track id={1} genres={genresMock} getLyric={getLyric} />
-  );
-
-  test("Check if on clicking called a function to get a lyric", () => {
-    track.find(".cover").simulate("click");
-    expect(getLyric).toHaveBeenCalledTimes(1);
   });
 });
